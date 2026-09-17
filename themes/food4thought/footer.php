caeffc15eb308b1d25d0a8f21227bddf648264e0
emmarisgaard <emmarisgaard@gmail.com>
2026-09-17 11:03:38 +0200

    Header, footer and css

--- themes/food4thought/footer.php
+++ themes/food4thought/footer.php
@@ -1,19 +1,73 @@
 <footer>
-		<h1 class="logoFooter"><a href="index.html">FOOD4THOUGHT</a></h1>
+
+		<h1 class="logoFooter">
+
+			<a href="index.html">
+				FOOD4THOUGHT
+			</a>
+
+		</h1>
+
+
 		<ul class="footerNav">
-			<li><a href="#">Support</a></li>
-			<li><a href="#">Cookie Policy</a></li>
-			<li><a href="#">Copyright</a></li>
+
+			<li>
+				<a href="#">
+					Support
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					Cookie Policy
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					Copyright
+				</a>
+			</li>
+
 		</ul>
+
+
 		<ul class="footerNav">
-			<li><a href="allRecipes.html">Recipes</a></li>
-			<li><a href="#">Foodstories & tips</a></li>
-			<li><a href="#">Tools & Recommendations</a></li>
-			<li><a href="#">Profile</a></li>
+
+			<li>
+				<a href="allRecipes.html">
+					Recipes
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					Foodstories & tips
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					Tools & Recommendations
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					Profile
+				</a>
+			</li>
+
 		</ul>
-		<div class="footerSearch"><i class="fa-solid fa-magnifying-glass"></i></div>
+
+
+		<div class="footerSearch">
+
+			<i class="fa-solid fa-magnifying-glass"></i>
+
+		</div>
+
 	</footer>
-    
 </body>
 
 </html>
--- themes/food4thought/front-page.php
+++ themes/food4thought/front-page.php
@@ -1,6 +1,48 @@
 <?php get_header(); ?>
 front page
+Mix the courgette ribbons with the salt in a large bowl. Massage with your hands for 30 seconds and then set aside for a few minutes.
 
+Gently squeeze the courgette and drain away any excess water.
+
+Place the peas in a large bowl and crush gently with a potato masher. Add the courgette ribbons to the bowl, then add the oil, lemon juice and zest, feta and chilli. Toss everything together to combine.
+
+Pile the pea mixture on top of the toast and serve immediately.Mix the courgette ribbons with the salt in a large bowl. Massage with your hands for 30 seconds and then set aside for a few minutes.
+
+Gently squeeze the courgette and drain away any excess water.
+
+Place the peas in a large bowl and crush gently with a potato masher. Add the courgette ribbons to the bowl, then add the oil, lemon juice and zest, feta and chilli. Toss everything together to combine.
+
+Pile the pea mixture on top of the toast and serve immediately.Mix the courgette ribbons with the salt in a large bowl. Massage with your hands for 30 seconds and then set aside for a few minutes.
+
+Gently squeeze the courgette and drain away any excess water.
+
+Place the peas in a large bowl and crush gently with a potato masher. Add the courgette ribbons to the bowl, then add the oil, lemon juice and zest, feta and chilli. Toss everything together to combine.
+
+Pile the pea mixture on top of the toast and serve immediately.Mix the courgette ribbons with the salt in a large bowl. Massage with your hands for 30 seconds and then set aside for a few minutes.
+
+Gently squeeze the courgette and drain away any excess water.
+
+Place the peas in a large bowl and crush gently with a potato masher. Add the courgette ribbons to the bowl, then add the oil, lemon juice and zest, feta and chilli. Toss everything together to combine.
+
+Pile the pea mixture on top of the toast and serve immediately.Mix the courgette ribbons with the salt in a large bowl. Massage with your hands for 30 seconds and then set aside for a few minutes.
+
+Gently squeeze the courgette and drain away any excess water.
+
+Place the peas in a large bowl and crush gently with a potato masher. Add the courgette ribbons to the bowl, then add the oil, lemon juice and zest, feta and chilli. Toss everything together to combine.
+
+Pile the pea mixture on top of the toast and serve immediately.Mix the courgette ribbons with the salt in a large bowl. Massage with your hands for 30 seconds and then set aside for a few minutes.
+
+Gently squeeze the courgette and drain away any excess water.
+
+Place the peas in a large bowl and crush gently with a potato masher. Add the courgette ribbons to the bowl, then add the oil, lemon juice and zest, feta and chilli. Toss everything together to combine.
+
+Pile the pea mixture on top of the toast and serve immediately.Mix the courgette ribbons with the salt in a large bowl. Massage with your hands for 30 seconds and then set aside for a few minutes.
+
+Gently squeeze the courgette and drain away any excess water.
+
+Place the peas in a large bowl and crush gently with a potato masher. Add the courgette ribbons to the bowl, then add the oil, lemon juice and zest, feta and chilli. Toss everything together to combine.
+
+Pile the pea mixture on top of the toast and serve immediately.
 
 <?php
 get_footer();
